5295c5f4ee6750958513dfd85a85ddb3771966f5
Egor Khromov <hromov.ego@yandex.ru>
2024-11-05 19:58:46 +0400

    Создаю новый репозиторий приложения учета книг с Spring Boot

---
+++ src/test/java/ru/khromov/library/LibraryBoot/LibraryBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.khromov.library.LibraryBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
